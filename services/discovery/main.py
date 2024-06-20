2a487ad9203840d6aeeb7b3639e43e813d07a391
Jens Skott <jens.skott@checkatrade.com>
2024-06-20 12:16:12 +0100

    port 8080 needed

--- services/discovery/main.py
+++ services/discovery/main.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     import uvicorn
 
     print("Starting discovery service...")
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
