2521adbc98d8d60187f7ad75adae29c3a5cb57b0
Jens Skott <jens.skott@checkatrade.com>
2024-06-21 23:02:50 +0100

    updated with depends for api

--- terraform/app_engine.tf
+++ terraform/app_engine.tf
@@ -7,21 +7,29 @@ resource "google_app_engine_application" "frontend" {
     oauth2_client_id     = google_iap_client.client.client_id
     oauth2_client_secret = google_iap_client.client.secret
   }
+
+  depends_on = [google_project_service.this["appengine.googleapis.com"]]
 }
 
 resource "google_iap_brand" "brand" {
   project           = var.project_id
   support_email     = "support@thoughtgears.co.uk"
   application_title = "Thoughtgears application"
+
+  depends_on = [google_project_service.this["iap.googleapis.com"]]
 }
 
 resource "google_iap_client" "client" {
   display_name = "Default Client"
   brand        = google_iap_brand.brand.name
+
+  depends_on = [google_project_service.this["iap.googleapis.com"]]
 }
 
 resource "google_iap_web_iam_member" "member" {
   project = var.project_id
   role    = "roles/iap.httpsResourceAccessor"
   member  = "domain:${local.github_owner}.co.uk"
+
+  depends_on = [google_project_service.this["iap.googleapis.com"]]
 }
