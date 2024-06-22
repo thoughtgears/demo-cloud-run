cb70bba824aa0500345eb8137b6faaba68f316c9
Jens Skott <jens.skott@checkatrade.com>
2024-06-22 08:38:15 +0100

    updated with custom builder

---
+++ resources/cloud_build/poll_spacelift/main.py
@@ -0,0 +1,87 @@
+import requests
+import os
+import sys
+import time
+import click
+
+
+def graphql_request(url, query, variables, token=""):
+    headers = {
+        "Content-Type": "application/json",
+    }
+    if token:
+        headers["Authorization"] = f"Bearer {token}"
+
+    response = requests.post(url, json={"query": query, "variables": variables}, headers=headers).json()
+    if "errors" in response:
+        raise Exception(f"GraphQL error: {', '.join(error['message'] for error in response['errors'])}")
+
+    return response["data"]
+
+
+def get_jwt(url, key_id, key_secret):
+    query = """
+    mutation ($keyId: ID!, $keySecret: String!) {
+      apiKeyUser(id: $keyId, secret: $keySecret) {
+        jwt
+      }
+    }
+    """
+    response_data = graphql_request(url, query, {"keyId": key_id, "keySecret": key_secret})
+    return response_data["apiKeyUser"]["jwt"]
+
+
+def get_run_status(url, key_id, key_secret, stack_id):
+    jwt = get_jwt(url, key_id, key_secret)
+
+    query = """
+    query ($id: ID!) {
+      stack(id: $id) {
+        runs {
+          isMostRecent
+          finished
+          state
+        }
+      }
+    }
+    """
+
+    response = graphql_request(url, query, {"id": stack_id}, jwt)
+    runs = response["stack"]["runs"]
+
+    for run in runs:
+        if run["isMostRecent"]:
+            return run["finished"], run["state"]
+
+    return None, "UNKNOWN"
+
+
+@click.command()
+@click.option("--spacelift-api-url", required=True, help="The Spacelift API URL.")
+@click.option("--spacelift-api-key-id", required=True, help="The Spacelift API key ID.")
+@click.option("--spacelift-api-key", required=True, help="The Spacelift API key secret.")
+@click.option("--spacelift-stack-id", required=True, help="The Spacelift stack ID.")
+def main(space_lift_api_url, spacelift_key_id, spacelift_key, spacelift_stack_id):
+    failure_states = {"FAILED", "SKIPPED", "STOPPED", "DISCARDED", "CANCELED"}
+
+    while True:
+        finished, state = get_run_status(space_lift_api_url, spacelift_key_id, spacelift_key, spacelift_stack_id)
+
+        if state in failure_states:
+            print(f"Run failed with state: {state}")
+            sys.exit(1)
+
+        if finished:
+            if state == "FINISHED":
+                print("Run finished successfully")
+                sys.exit(0)
+            else:
+                print(f"Run finished with state: {state}")
+                sys.exit(1)
+
+        print(f"Run is in progress with state: {state}. Checking again in 30 seconds...")
+        time.sleep(30)
+
+
+if __name__ == "__main__":
+    main()
--- terraform/cloud_build.tf
+++ terraform/cloud_build.tf
@@ -1,5 +1,8 @@
 /*
-  Setup the Cloud Build connection to GitHub
+  Create the secrets to support the Cloud Build connection to GitHub
+  INFO: The pat is a classic pat with no expiration and access to all
+  repositories, it has repo, red:user and read:org
+  https://cloud.google.com/build/docs/automating-builds/github/connect-repo-github?generation=2nd-gen
  */
 resource "google_secret_manager_secret" "github_pat_thoughtgears" {
   project   = var.project_id
@@ -28,6 +31,10 @@ resource "google_secret_manager_secret_iam_policy" "policy" {
   policy_data = data.google_iam_policy.p4sa-secretAccessor.policy_data
 }
 
+/*
+  Create the Cloud Build connection to GitHub with the help of the PAT
+  This has to be v2 since the v1 dont support programmatic creation
+ */
 resource "google_cloudbuildv2_connection" "github_thoughtgears" {
   project  = var.project_id
   location = var.region
@@ -50,16 +57,95 @@ resource "google_cloudbuildv2_repository" "demo_cloud_run" {
 }
 
 /*
-  Setup the Cloud Build trigger
+  Setup the Cloud Build service account and the permissions required for the build
+  INFO: These are broader then needed, but for the purpose of the demo, we will give
+  the service account admin, in the real world, you would want to limit these permissions
  */
-resource "google_cloudbuild_trigger" "this" {
-  project  = var.project_id
-  location = var.region
-  name     = "${local.github_repo}-deploy"
+resource "google_service_account" "build_demo_cloud_build" {
+  project    = var.project_id
+  account_id = "build-demo-cloud-run"
+}
 
-  substitutions = {
+resource "google_project_iam_member" "build_demo_cloud_build_act_as" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.build_demo_cloud_build.email}"
+}
+
+resource "google_project_iam_member" "build_demo_cloud_build_logs_writer" {
+  project = var.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.build_demo_cloud_build.email}"
+}
+
+resource "google_project_iam_member" "build_demo_cloud_build_artifact_registry_admin" {
+  project = var.project_id
+  role    = "roles/artifactregistry.admin"
+  member  = "serviceAccount:${google_service_account.build_demo_cloud_build.email}"
+}
+
+resource "google_project_iam_member" "build_demo_cloud_build_run_admin" {
+  project = var.project_id
+  role    = "roles/run.admin"
+  member  = "serviceAccount:${google_service_account.build_demo_cloud_build.email}"
+}
+
+resource "google_project_iam_member" "build_demo_cloud_build_app_engine_admin" {
+  project = var.project_id
+  role    = "roles/appengine.appAdmin"
+  member  = "serviceAccount:${google_service_account.build_demo_cloud_build.email}"
+}
+
+resource "google_project_iam_member" "build_demo_cloud_build_secret_manager_access" {
+  project = var.project_id
+  role    = "roles/secretmanager.secretAccessor"
+  member  = "serviceAccount:${google_service_account.build_demo_cloud_build.email}"
+}
+
+resource "google_project_iam_member" "build_demo_cloud_build_storage_admin" {
+  project = var.project_id
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.build_demo_cloud_build.email}"
+}
+
+/*
+  Setup the Cloud Build Triggers to deploy the Cloud Run services
+  INFO: The trigger will be triggered on a push to the main branch
+  and will use the cloudbuild.yaml file in each of the services directories
+  to deploy the services.
+
+  If any of the files altered in the commit pass the ignoredFiles filter and
+  includedFiles is empty, then as far as this filter is concerned, we should
+  trigger the build. If any of the files altered in the commit pass the ignoredFiles
+  filter and includedFiles is not empty, then we make sure that at least one of
+  those files matches a includedFiles glob. If not, then we do not trigger a build.
+ */
+locals {
+  services = {
+    "discovery" = {
+      name = "discovery"
+    }
+    "ipam" = {
+      name = "ipam"
+    }
+    "backend" = {
+      name = "backend"
+    }
+  }
+
+  global_substitutions = {
     _SPACELIFT_API_URL = "https://thoughtgears.app.spacelift.io/graphql"
   }
+}
+
+resource "google_cloudbuild_trigger" "services" {
+  for_each = local.services
+
+  project  = var.project_id
+  location = var.region
+  name     = "${each.value.name}-deploy"
+
+  substitutions = merge(local.global_substitutions, lookup(each.value, "substitutions", {}))
 
   ignored_files = [
     "README.md",
@@ -71,7 +157,12 @@ resource "google_cloudbuild_trigger" "this" {
     "terraform/**",
     ".github/**",
     "firebase/**",
-    "emulator/**"
+    "emulator/**",
+    "resources/**"
+  ]
+
+  included_files = [
+    "services/${each.value.name}/**"
   ]
 
   repository_event_config {
@@ -82,7 +173,60 @@ resource "google_cloudbuild_trigger" "this" {
   }
 
   service_account    = google_service_account.build_demo_cloud_build.id
-  filename           = "cloudbuild.yaml"
+  filename           = "services/${each.value.name}/cloudbuild.yaml"
+  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+
+  depends_on = [
+    google_project_iam_member.build_demo_cloud_build_act_as,
+    google_project_iam_member.build_demo_cloud_build_app_engine_admin,
+    google_project_iam_member.build_demo_cloud_build_artifact_registry_admin,
+    google_project_iam_member.build_demo_cloud_build_logs_writer,
+    google_project_iam_member.build_demo_cloud_build_run_admin,
+    google_project_iam_member.build_demo_cloud_build_secret_manager_access
+  ]
+}
+
+/*
+  Build trigger for a custom build step that we should use in our Service runs
+  This will build a custom docker container and push to the Artifact Registry
+  platform repo that can be reused in the services cloud build steps
+ */
+resource "google_cloudbuild_trigger" "poll_spacelift" {
+  for_each = local.services
+
+  project  = var.project_id
+  location = var.region
+  name     = "poll-spacelift-deploy"
+
+  substitutions = merge(local.global_substitutions, lookup(each.value, "substitutions", {}))
+
+  ignored_files = [
+    "README.md",
+    "LICENSE",
+    ".gitignore",
+    "docker-compose.yml",
+    "Makefile",
+    "pyproject.toml",
+    "terraform/**",
+    ".github/**",
+    "firebase/**",
+    "emulator/**",
+    "services/**"
+  ]
+
+  included_files = [
+    "resources/cloud_build/poll_spacelift/**"
+  ]
+
+  repository_event_config {
+    repository = google_cloudbuildv2_repository.demo_cloud_run.id
+    push {
+      branch = "^main$"
+    }
+  }
+
+  service_account    = google_service_account.build_demo_cloud_build.id
+  filename           = "resources/cloud_build/poll_spacelift/cloudbuild.yaml"
   include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
 
   depends_on = [
