c3381c1d6fae7f7f681abbd3207b809d5f84f053
Jens Skott <jens.skott@checkatrade.com>
2024-06-22 09:52:03 +0100

    simplifed the cloud build spacelift

---
+++ resources/cloud_build/spacelift/README.md
@@ -0,0 +1,16 @@
+# Spacelift
+
+This cloud build builder will query spacelift for stack runs to ensure that the terraform runes are successful.
+This can be used to ensure that the terraform runs are successful before building and deploying applications or
+other cloud build steps that require infrastructure to be in place.
+
+## Usage
+
+```yaml
+steps:
+  - name: 'gcr.io/$PROJECT_ID/spacelift'
+    id: 'spacelift'
+    env:
+      - 'URL=$_SPACELIFT_API_URL'
+    secretEnv: [ 'SPACELIFT_API_KEY', 'SPACELIFT_KEY_ID', 'SPACELIFT_STACK_ID' ]
+```
--- resources/cloud_build/spacelift/spacelift.bash
+++ resources/cloud_build/spacelift/spacelift.bash
@@ -1,10 +1,5 @@
 #!/bin/bash
-URL=${SPACELIFT_API_URL}
-KEY_ID=${SPACELIFT_API_KEY_ID}
-API_KEY=${SPACELIFT_API_KEY}
-STACK_ID=${SPACELIFT_STACK_ID}
-
 echo "Running /app/main.py with arguments:"
-echo "--url=${URL} --key-id=${KEY_ID} --api-key=${API_KEY} --stack-id=${STACK_ID}"
+echo "--url=${SPACELIFT_URL} --key-id=${SPACELIFT_KEY_ID} --api-key=${SPACELIFT_API_KEY} --stack-id=${SPACELIFT_STACK_ID}"
 
-python /app/main.py --url="${URL}" --key-id="${KEY_ID}" --api-key="${API_KEY}" --stack-id="${STACK_ID}"
+python /app/main.py --url="${SPACELIFT_URL}" --key-id="${SPACELIFT_KEY_ID}" --api-key="${SPACELIFT_API_KEY}" --stack-id="${SPACELIFT_STACK_ID}"
